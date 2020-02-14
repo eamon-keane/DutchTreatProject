c400066ee5a7991e87b97b52c5c438e61cc85aa4
Eamon Keane <e.keane10@gmail.com>
2020-02-14 08:39:29 +0000

    jquery added

--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,7 @@ namespace DutchTreat
         {
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseNodeModules();
         }
     }
 }
---
+++ wwwroot/js/index.js
@@ -0,0 +1,34 @@
+﻿
+$(document).ready(function () {
+    var x = 0;
+    var s = "";
+
+
+    console.log("Hello pluralsight");
+
+    var theForm = $("#theForm");
+
+    theForm.hide();
+
+    var button = $("#buyButton");
+
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+
+    productInfo.on("click", function () {
+        console.log("you clicked on" + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle(1000);
+    });
+
+
+
+});
